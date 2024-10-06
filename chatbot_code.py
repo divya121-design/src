024998dde348d0b2ef1ea7be45d489419e266e0c
Amith Vardhan Reddy Surasani <132837289+amithvardhan99@users.noreply.github.com>
2024-10-06 16:40:20 -0400

    chatbot_code

--- chatbot_code.py
+++ chatbot_code.py
@@ -28,11 +28,12 @@ def get_chat_response(messages):
     except Exception as e:
         return f"Error: {e}"
 
-
+"""
 print("Hi! Welcome to DUTA. How can I help you?")
 print("Please enter your message so that I will process it and generate the output.")
 print("Type 'exit' or 'quit' to quit the chatbot.")
 print()
+"""
 
 chatbot = []
 msg = []
@@ -40,31 +41,35 @@ msg = []
 
 while True:
     single_message = {}
-    print("Role    : User")
+    # print("Role    : User")
     entered_text = input("Content: ").strip()
     print()
 
     if entered_text.lower() in ["exit", "quit"]:
-        print("Your response has ended.")
+        break
 
     user_message = {"role": "user", "content": entered_text}
     msg.append(user_message)
 
-    prompt = msg[-5:]  # last 5 messages
+    prompt = msg[-10:]  # last 5 messages
     response = get_chat_response(prompt)
-    output_text = response.choices[0].message['content'].strip()
+    output_text = response.choices[0].message["content"].strip()
 
     assistant_message = {"role": "assistant", "content": output_text}
     msg.append(assistant_message)
 
-    print("Role    : Assistant")
-    print("Content : ", output_text)
-    print()
+    # print("Role    : Assistant")
+    # print("Content : ", output_text)
+    # print()
 
-    print("Related Sources:")
+    single_message = [user_message, assistant_message]
+    chatbot += single_message
 
-    single_message["messages"] = [user_message, assistant_message]
-    chatbot.append(single_message)
-
+ctr = 0
 with open("chatbot.json", 'w') as fp:
-    json.dump(chatbot, fp)
+    for i in chatbot:
+        json.dump(i, fp)     
+        ctr += 1
+        if ctr % 2 == 0:
+            fp.write("\n")
+        fp.write("\n")
